af5953aeae888ff0dae55852ccac1e2fc512a016
DJLuigi00 <102384339+DJLuigi00@users.noreply.github.com>
2024-04-01 13:13:49 -0400

    NEON

--- Assets/Scripts/Battle/Axe.cs
+++ Assets/Scripts/Battle/Axe.cs
@@ -6,6 +6,7 @@ using static UnityEditor.IMGUI.Controls.PrimitiveBoundsHandle;
 public class Axe : MonoBehaviour
 {
     public Battle battle;
+    public Animator animator;
     public void PressAxe()
     {
         if (battle.PlayerTurn == true)
@@ -13,6 +14,7 @@ public class Axe : MonoBehaviour
             if (battle.axe == true)
             {
                 battle.playerDamage = 50;
+                animator.SetTrigger("DoSlash");
                 battle.enemyTurnCount -= battle.playerDamage;
                 battle.axe = false;
                 battle.PlayerTurn = false;
