f9373a5db91f466b0c433f6f2491bece15564c64
HauserCGearSpin <158326781+HauserCGearSpin@users.noreply.github.com>
2024-04-12 12:33:00 -0400

    4/12/24 - 1 - Cole

--- Assets/Scripts/DataPeresistance/Data/GameData.cs
+++ Assets/Scripts/DataPeresistance/Data/GameData.cs
@@ -7,8 +7,13 @@ public class GameData
 {
     // values defined in constructor will be default values
     // game starts with these default values when there's no data to load
+
+    public int deathCount;
+    public int winCount;
+
     public GameData()
     {
-
+        this.deathCount = 0;
+        this.winCount = 0;
     }
 }
---
+++ Assets/Scripts/StatsDisplay.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatsDisplay : MonoBehaviour, IDataPersistence
+{
+    [HideInInspector] public int deathCount;
+    [HideInInspector] public int winCount;
+
+    [SerializeField] private TMP_Text deathCountTxt;
+    [SerializeField] private TMP_Text winCountTxt;
+
+    private void Start()
+    {
+        deathCountTxt.text = this.deathCount.ToString();
+        winCountTxt.text = this.deathCount.ToString();
+    }
+
+    public void LoadData(GameData data)
+    {
+        this.deathCount = data.deathCount;
+        this.winCount = data.winCount;
+    }
+    public void SaveData(ref GameData data)
+    { 
+        data.deathCount = this.deathCount;
+        data.winCount = this.winCount;
+    }    
+}
